3174695db8fb819b424ba55a3fa88333cf2f25c0
slavinp <slavinp@cs.man.ac.uk>
2014-08-01 14:01:21 +0100

    Multi-Key support added.

--- peakmem.c
+++ peakmem.c
@@ -10,12 +10,19 @@
 #include <signal.h>
 
 extern const char *const sys_siglist[];
-const int HZ = 4;
-const char *const key = "VmSize: %lld kB";
+const int HZ = 4, KEYCOUNT = 2;
 
 int cstate = 0, status =0;
 struct timeval tv[2];	// START, LAST
 enum { START, LAST };
+enum { SZ, RSS };
+
+
+struct keystate{
+	long long last, avg, hi;
+	unsigned int hi_hours, hi_mins, hi_secs;
+	const char *const key;
+};
 
 
 long long pollProc(const char *const, const char *const);
@@ -25,12 +32,18 @@ void sigchld_handler(int);
 int main(int argc, char *argv[])
 {
 	char statfile[24], *pname = NULL;
-	long long last = -1L, avg = 0L, hi = -1L;
 	unsigned int count = 0, linelen = 0, prevlen = 0;
 	unsigned int hours = 0, mins = 0, secs = 0;
-	unsigned int hi_hours = 0, hi_mins = 0, hi_secs = 0;
 	time_t hitime;
 	pid_t pid;
+	int key;
+
+	struct keystate states[2] = {
+		{-1L, 0L, -1L, 0, 0, 0, "VmSize: %lld kB"},	// SZ
+		{-1L, 0L, -1L, 0, 0, 0, "VmRSS: %lld kB"}	// RSS
+	};
+
+//	printf("%d: %lld %lld %lld %u %u %u\n", RSS, states[RSS].last, states[RSS].avg, states[RSS].hi, states[RSS].hi_hours, states[RSS].hi_mins, states[RSS].hi_secs);
 
 	if(argc == 1){
 		fprintf(stderr, "Usage: %s <program>\n", argv[0]);
@@ -66,23 +79,24 @@ int main(int argc, char *argv[])
 
 	while(cstate){
 
-		last = pollProc(statfile, key);
-		gettimeofday(&tv[LAST], NULL);
-
-		time_t deltasec = tv[LAST].tv_sec - tv[START].tv_sec;
-
-//		lo = (last < lo) ? lotime=deltasec, last : lo;
-//		hi = (last > hi) ? hitime=deltasec, last : hi;
-
-		if(last > hi){
-			hi = last;
-			hitime = deltasec;
-			hi_hours = hitime/3600;
-			hi_mins = (hitime-(3600*hours))/60;
-			hi_secs = hitime % 60;
+		for(key=0; key<KEYCOUNT; key++){
+			states[key].last = pollProc(statfile, states[key].key);
 		}
 
-		avg += (last-avg)/(count+1);
+		gettimeofday(&tv[LAST], NULL);
+		time_t deltasec = tv[LAST].tv_sec - tv[START].tv_sec;
+
+		for(key=0; key<KEYCOUNT; key++){
+			if(states[key].last > states[key].hi){
+				states[key].hi = states[key].last;
+				hitime = deltasec;
+				states[key].hi_hours = hitime/3600;
+				states[key].hi_mins = (hitime-(3600*hours))/60;
+				states[key].hi_secs = hitime % 60;
+			}
+
+			states[key].avg += (states[key].last-states[key].avg)/(count+1);
+		}
 		count++;
 
 		hours = deltasec/3600;
@@ -91,7 +105,7 @@ int main(int argc, char *argv[])
 
 		prevlen = linelen;
 		linelen = printf("\r\x1B[31m[PeakMem] %s (%d)\x1B[0m    [ HI: %lld kB  (%02d:%02d:%02d)   AVG: %lld kB    LAST: %lld kB ]   \x1B[32m(%02d:%02d:%02d)\x1B[0m" \
-				, pname, pid, hi, hi_hours, hi_mins, hi_secs, avg, last, hours, mins, secs);
+				, pname, pid, states[RSS].hi, states[RSS].hi_hours, states[RSS].hi_mins, states[RSS].hi_secs, states[RSS].avg, states[RSS].last, hours, mins, secs);
 		if(linelen < prevlen){
 			printf("%*.s", prevlen-linelen, " ");
 		}
@@ -149,3 +163,4 @@ void sigchld_handler(int signo)
 
 	cstate = 0;
 }
+
